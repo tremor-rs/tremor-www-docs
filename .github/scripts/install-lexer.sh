48823b1f5cd4cef989f5fa568d657273d679c99e
Heinz N. Gies <heinz@licenser.net>
2020-01-29 14:04:41 +0100

    Follow python workflow

--- .github/scripts/install-lexer.sh
+++ .github/scripts/install-lexer.sh
@@ -1,9 +1,4 @@
 
-
-deps() {
-    sudo pip3 install setuptools
-}
-
 fetch() {
     # To replace with a clone once the repo is open source!
     cp -r .github/vendor/lexer tremor-mkdocs-lexer
@@ -13,7 +8,5 @@ install() {
     cd tremor-mkdocs-lexer
     python3 ./setup.py install
 }
-
-deps
 fetch
 install
